71bc3cc5716701c61956e0e1d8593a96bd77481e
oluciams <oluciams84@gmail.com>
2023-04-05 16:47:26 -0500

    add CRUD

--- src/pages/api/tasks/[id].ts
+++ src/pages/api/tasks/[id].ts
@@ -1,16 +1,59 @@
+import { Console } from "console";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { conn } from "../../../utils/database";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   
-  const { method } = req
+  const { method, query, body } = req
+
   switch (method) {
     case "GET":
-      return res.json("getting a Task");
+      try {
+        const text = "SELECT * FROM tasks WHERE id = $1"
+        const values = [query.id]
+        const response = await conn.query(text, values)
+
+        if (response.rows.length === 0) 
+          return res.status(404).json({ message: "Task not found" })
+        
+        return res.status(200).json(response.rows[0])
+        
+      } catch (error: any) {
+        return res.status(500).json({ error: error.message });
+      }
+     
     case "PUT":
-      return res.json("updating a Task");
+      try {
+        const {title, description} = body
+        const text = "UPDATE tasks SET title = $1, description = $2 WHERE id = $3 RETURNING *";
+        const values = [title, description, query.id];
+        const response = await conn.query(text, values);
+
+        if (response.rows.length === 0)
+          return res.status(404).json({ message: "Task not found" });
+
+        return res.status(200).json(response);
+      } catch (error: any) {
+        return res.status(500).json({ error: error.message });
+      }
+     
+      
     case "DELETE":
-      return res.json("deleting a Task");
+      try {
+        const text = "DELETE FROM tasks WHERE id = $1 RETURNING *";
+        const values = [query.id];
+        const response = await conn.query(text, values);
+
+        if (response.rowCount === 0)
+          return res.status(404).json({ message: "Task not found" })
+        
+        return res.status(200).json(response.rows[0])
+        
+      } catch (error: any) {
+        return res.status(500).json({ error: error.message });
+      }
+
     default:
       return res.status(400).json("method not allowed");
   }
--- src/pages/api/tasks/index.ts
+++ src/pages/api/tasks/index.ts
@@ -1,5 +1,5 @@
-import { conn } from "@/utils/database";
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next"
+import { conn } from "../../../utils/database"
 
 
 // eslint-disable-next-line import/no-anonymous-default-export
@@ -9,15 +9,27 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   switch (method) {
     case "GET":
-      return res.status(200).json("getting Tasks")
+      try {
+        const query = "SELECT * FROM tasks";
+        const response = await conn.query(query)
+        return res.status(200).json(response.rows)
+      } catch (error: any) {
+        return res.status(400).json({ error: error.message })
+      }
+
     case "POST":
-      const { title, description } = body;
-      const query = "INSERT INTO tasks(title, description) VALUES ($1, $2) RETURNING *";
-      const values = [title, description];
-      const response = await conn.query(query, values)
-      console.log(response)
-      return res.status(200).json(response.rows[0]);
-    
+      try {
+        const { title, description } = body
+        const query = "INSERT INTO tasks(title, description) VALUES ($1, $2) RETURNING *";
+        const values = [title, description]
+        const response = await conn.query(query, values)
+       
+        return res.status(200).json(response.rows[0])
+        
+      } catch (error: any) {
+        return res.status(400).json({ error: error.message })
+      }
+
     default:
       return res.status(400).json("invalid Tasks")
   }
